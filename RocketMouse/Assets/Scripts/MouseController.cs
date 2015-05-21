d036d22f99fb943658c6647cf2f01bd00bb0d771
李亚科 <me@liyake.com>
2015-05-21 16:01:14 +0800

    Almost done the RocketMouse

--- RocketMouse/Assets/Scripts/MouseController.cs
+++ RocketMouse/Assets/Scripts/MouseController.cs
@@ -6,9 +6,32 @@ public class MouseController : MonoBehaviour {
 	public float jetpackForce = 75.0f;
 	public float forwardMovementSpeed = 3.0f;
 
+	public Transform groundCheckTransform;
+	private bool grounded;
+	public LayerMask groundCheckLayerMask;
+	Animator animator;
+
+	public ParticleSystem jetpack;
+
+	private bool dead = false;
+
+	private uint coins = 0;
+
+	public Texture2D coinIconTexture;
+
+	public GUIStyle restartButtonStyle;
+
+	public AudioClip coinCollectSound;
+
+	public AudioSource jetpackAudio;
+	public AudioSource footstepsAudio;
+
+	public ParallaxScroll parallax;
+
 	// Use this for initialization
 	void Start () {
-	
+		animator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
@@ -19,13 +42,103 @@ public class MouseController : MonoBehaviour {
 	void FixedUpdate()
 	{
 		bool jetpackActive = Input.GetButton ("Fire1");
+		if(!jetpackActive)
+			jetpackActive = Input.GetButton ("Jump");
+
+		jetpackActive = jetpackActive && !dead;
 
 		if (jetpackActive) {
 			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, jetpackForce));
 		}
 
-		Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
-		newVelocity.x = forwardMovementSpeed;
-		GetComponent<Rigidbody2D>().velocity = newVelocity;
+		if (!dead) {
+			Vector2 newVelocity = GetComponent<Rigidbody2D> ().velocity;
+			newVelocity.x = forwardMovementSpeed;
+			GetComponent<Rigidbody2D> ().velocity = newVelocity;
+		}
+
+		UpdateGroundedStatus ();
+		AdjustJetpack (jetpackActive);
+		AdjustFootstepsAndJetpackSound (jetpackActive);
+
+		parallax.offset = transform.position.x;
+	}
+
+	void UpdateGroundedStatus()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+
+		animator.SetBool ("grounded", grounded);
+	}
+
+	void AdjustJetpack(bool jetpackActive)
+	{
+		jetpack.enableEmission = !grounded;
+		jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f;
+	}
+
+	void AdjustFootstepsAndJetpackSound(bool jetpackActive)
+	{
+		footstepsAudio.enabled = !dead && grounded;
+
+		jetpackAudio.enabled = !dead && !grounded;
+		jetpackAudio.volume = jetpackActive ? 1.0f : 0.5f;
+	}
+
+	void HitByLaser(Collider2D laserCollider)
+	{
+		if (!dead)
+			laserCollider.gameObject.GetComponent<AudioSource>().Play ();
+
+		dead = true;
+		animator.SetBool ("dead", true);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.CompareTag ("Coins"))
+			CollectCoin (collider);
+		else
+			HitByLaser (collider);
+	}
+
+	void CollectCoin(Collider2D coinCollider)
+	{
+		coins++;
+		Destroy (coinCollider.gameObject);
+
+		AudioSource.PlayClipAtPoint (coinCollectSound, transform.position);
+	}
+
+	
+	void OnGUI()
+	{
+		DisplayCoinsCount ();
+		DisplayRestartButton ();
+	}
+
+	void DisplayCoinsCount()
+	{
+		Rect coinIconRect = new Rect (10, 10, 32, 32);
+		GUI.DrawTexture (coinIconRect, coinIconTexture);
+
+		GUIStyle style = new GUIStyle ();
+		style.fontSize = 30;
+		style.fontStyle = FontStyle.Bold;
+		style.normal.textColor = Color.yellow;
+
+		Rect labelRect = new Rect (coinIconRect.xMax, coinIconRect.y, 60, 32);
+		GUI.Label (labelRect, coins.ToString (), style);
+	}
+
+	void DisplayRestartButton()
+	{
+		if (dead && grounded) {
+			Rect buttonRect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.30f, Screen.height * 0.1f);
+			if(GUI.Button(buttonRect, "Tap to restart!"/*,restartButtonStyle*/))
+			{
+				Application.LoadLevel(Application.loadedLevelName);
+			}
+		}
 	}
 }
---
+++ RocketMouse/Assets/Scripts/ParallaxScroll.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroll : MonoBehaviour {
+	public Renderer backgound;
+	public Renderer foregroud;
+
+	public float backgroundSpeed = 0.02f;
+	public float foregroundSpeed = 0.06f;
+
+
+	public float offset = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float backgroundOffset = offset * backgroundSpeed;
+		float foregroundOffset = offset * foregroundSpeed;
+
+		backgound.material.mainTextureOffset = new Vector2 (backgroundOffset, 0);
+		foregroud.material.mainTextureOffset = new Vector2 (foregroundOffset, 0);
+	}
+}
