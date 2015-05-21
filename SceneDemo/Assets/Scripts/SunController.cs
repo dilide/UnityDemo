52b5bb43e032d4c57b88d093765337c86a2bf740
李亚科 <me@liyake.com>
2015-05-21 17:45:16 +0800

    Add new project

---
+++ SceneDemo/Assets/Scripts/SunController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunController : MonoBehaviour,IPlayer {
+	bool isPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	public void Play()
+	{
+		isPlaying = !isPlaying;
+		Debug.Log ("Play trigger...");
+	}
+}
